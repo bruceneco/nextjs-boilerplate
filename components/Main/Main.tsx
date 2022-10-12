741f98a49ce0f54835eeba463d7e42aa6ce1f257
Bruce Neco <bruce.neco3@outlook.com>
2022-10-12 18:29:46 -0300

    feat(component:Main): add storybook

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -1,16 +1,17 @@
 import * as S from './Main.styles'
 
-const Main: React.FC = () => {
+const Main = ({
+  title = 'NextJS Boilerplate',
+  description = 'Typescript, ReactJS, NextJS & Styled Components'
+}) => {
   return (
     <S.Wrapper>
       <S.Logo
         src={'/img/logo.svg'}
         alt={'Atom image, with "React Avançado" typed'}
       />
-      <S.Title>NextJS Boilerplate</S.Title>
-      <S.Description>
-        Typescript, ReactJS, NextJS & Styled Components
-      </S.Description>
+      <S.Title>{title}</S.Title>
+      <S.Description>{description}</S.Description>
       <S.Illustration
         src={'/img/hero-illustration.svg'}
         alt={'A developer in front of a screen with  code.'}
