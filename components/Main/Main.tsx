02bb434539038666e326246865ed3d05fa1851b0
Bruce Neco <bruce.neco3@outlook.com>
2022-10-12 17:03:29 -0300

    feat(component:Main): add content

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -1,8 +1,21 @@
+import * as S from './Main.styles'
+
 const Main: React.FC = () => {
   return (
-    <main>
-      <h1>NextJS Boilerplate</h1>
-    </main>
+    <S.Wrapper>
+      <S.Logo
+        src={'/img/logo.svg'}
+        alt={'Atom image, with "React Avançado" typed'}
+      />
+      <S.Title>NextJS Boilerplate</S.Title>
+      <S.Description>
+        Typescript, ReactJS, NextJS & Styled Components
+      </S.Description>
+      <S.Illustration
+        src={'/img/hero-illustration.svg'}
+        alt={'A developer in front of a screen with  code.'}
+      />
+    </S.Wrapper>
   )
 }
 
